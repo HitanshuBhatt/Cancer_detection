ce66dfc2ba1f693b156b128e2abd376e3e095a82
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-03-24 18:03:38 -0600

    sample frontend creation for presentation purposes only

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,13 @@ app.include_router(router)
 @app.get("/")
 def home():
     return {"message": "Lung Cancer Detection API Running"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
